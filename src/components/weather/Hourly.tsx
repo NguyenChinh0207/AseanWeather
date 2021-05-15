486ba81b9d7ebe151f91fc2c2e580e3afca4a443
dinhtuanpk <dinhtuanpk@gmail.com>
2021-05-15 10:46:07 +0700

    fix hourly daily

--- src/components/weather/Hourly.tsx
+++ src/components/weather/Hourly.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./hourly.scss";
 import {
   Accordion,
@@ -42,6 +42,7 @@ const ContextAwareToggle: React.FC<IContextAwareToggle> = ({
 };
 
 const Hourly = () => {
+  const [timeNow, setTimeNow] = useState(new Date().getHours());
   const propsData = useSelector(
     (state: RootStateOrAny) => state.weatherReducer
   );
@@ -59,120 +60,123 @@ const Hourly = () => {
       {propsData.weatherHourly.map((item: any) => {
         let d = new Date(item.time.substring(0, 10));
         let date =
-          d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();
-        return (
-          <div className="container Box-cha" key={item.time_epoch}>
-            <div className="hourly-card-nfl-header ">
-              <h2 className="date">
-                <span>{date}</span>
-                <span className="sub">{item.time.substring(11, 16)}</span>
-              </h2>
-              <div className="d-flex align-items-center">
-                <img
-                  alt=""
-                  className="weather-icon icon-two"
-                  width="128px"
-                  height="128px"
-                  data-eager=""
-                  src={item.condition.icon}
-                />
-                <div className="temp metric">{item.temp_c}°</div>
-              </div>
-              <span className="real-feel">
-                Cảm thấy như{" "}
-                {item.feelslike_c}°C
-              </span>
-              <div className="precip">
-                <img
-                  alt="rain drop"
-                  className="precip-icon"
-                  src="/assets/icons/rainss.png"
-                />
-                {item.precip_mm}mm
-              </div>
-              <ContextAwareToggle
-                eventKey={item.time_epoch}
-              ></ContextAwareToggle>
-            </div>
-            <Accordion.Collapse eventKey={item.time_epoch}>
-              <Card.Body>
-                <div className="box-con">
-                  <div className="forecast-container">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/wind.png" alt="" />
-                          {"  "}
-                          Tốc độ gió
-                          </th>
-                          <td scope="col">{item.wind_kph} km/h</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/windgust.png" alt="" />
-                          {"  "}
-                          Gió mạnh
-                          </th>
-                          <td scope="col">{item.gust_kph} km/h</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/compass.png" alt="" />
-                          {"  "}
-                            Hướng gió</th>
-                          <td scope="col">{item.wind_degree} °</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/UV.png" alt="" />
-                          {"  "}
-                            Chỉ số UV</th>
-                          <td scope="col">{item.uv}</td>
-                        </tr>
-                      </thead>
-                    </table>
+          d.getDate() + "-" + (d.getMonth() + 1) + "-" + d.getFullYear();       
+          if(item.time.substring(11, 13) >= timeNow){
+            return (
+              <div className="container Box-cha" key={item.time_epoch}>
+                {console.log("item",(item.time.substring(11, 13) > 22))}
+                <div className="hourly-card-nfl-header ">
+                  <h2 className="date">
+                    <span>{date}</span>
+                    <span className="sub">{item.time.substring(11, 16)}</span>
+                  </h2>
+                  <div className="d-flex align-items-center">
+                    <img
+                      alt=""
+                      className="weather-icon icon-two"
+                      width="128px"
+                      height="128px"
+                      data-eager=""
+                      src={item.condition.icon}
+                    />
+                    <div className="temp metric">{item.temp_c}°</div>
                   </div>
-                  <div className="forecast-container">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/chephu.png" alt="" />
-                          {"  "}
-                            Độ che phủ mây</th>
-                          <td scope="col">{item.cloud} %</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/eye.png" alt="" />
-                          {"  "}
-                            Tầm nhìn xa</th>
-                          <td scope="col">{item.vis_km} km/h</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/doam.png" alt="" />
-                          {"   "}
-                            Độ ẩm</th>
-                          <td scope="col">{item.humidity} %</td>
-                        </tr>
-                        <tr>
-                          <th scope="col">
-                          <img src="/assets/icons/rain.png" alt="" />
-                          {"  "}
-                            Lượng mưa</th>
-                          <td scope="col">{item.precip_mm} mm</td>
-                        </tr>
-                      </thead>
-                    </table>
+                  <span className="real-feel">
+                    Cảm thấy như{" "}
+                    {item.feelslike_c}°C
+                  </span>
+                  <div className="precip">
+                    <img
+                      alt="rain drop"
+                      className="precip-icon"
+                      src="/assets/icons/rainss.png"
+                    />
+                    {item.precip_mm}mm
                   </div>
-                  
+                  <ContextAwareToggle
+                    eventKey={item.time_epoch}
+                  ></ContextAwareToggle>
                 </div>
-              </Card.Body>
-            </Accordion.Collapse>
-          </div>
-        );
+                <Accordion.Collapse eventKey={item.time_epoch}>
+                  <Card.Body>
+                    <div className="box-con">
+                      <div className="forecast-container">
+                        <table className="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/wind.png" alt="" />
+                              {"  "}
+                              Tốc độ gió
+                              </th>
+                              <td scope="col">{item.wind_kph} km/h</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/windgust.png" alt="" />
+                              {"  "}
+                              Gió mạnh
+                              </th>
+                              <td scope="col">{item.gust_kph} km/h</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/compass.png" alt="" />
+                              {"  "}
+                                Hướng gió</th>
+                              <td scope="col">{item.wind_degree} °</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/UV.png" alt="" />
+                              {"  "}
+                                Chỉ số UV</th>
+                              <td scope="col">{item.uv}</td>
+                            </tr>
+                          </thead>
+                        </table>
+                      </div>
+                      <div className="forecast-container">
+                        <table className="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/chephu.png" alt="" />
+                              {"  "}
+                                Độ che phủ mây</th>
+                              <td scope="col">{item.cloud} %</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/eye.png" alt="" />
+                              {"  "}
+                                Tầm nhìn xa</th>
+                              <td scope="col">{item.vis_km} km/h</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/doam.png" alt="" />
+                              {"   "}
+                                Độ ẩm</th>
+                              <td scope="col">{item.humidity} %</td>
+                            </tr>
+                            <tr>
+                              <th scope="col">
+                              <img src="/assets/icons/rain.png" alt="" />
+                              {"  "}
+                                Lượng mưa</th>
+                              <td scope="col">{item.precip_mm} mm</td>
+                            </tr>
+                          </thead>
+                        </table>
+                      </div>
+                      
+                    </div>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </div>
+            );
+          }       
       })}
     </Accordion>
   );
